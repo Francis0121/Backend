d6b4168984e8bfd755ff7a8b6dcbf45b6bcc3b44
Francis0121 <tjdrms0121@gmail.com>
2016-04-19 23:15:20 +0900

    Set Property Information

--- src/main/java/me/ppangya/wiki/framework/config/RootApplicationConfig.java
+++ src/main/java/me/ppangya/wiki/framework/config/RootApplicationConfig.java
@@ -1,8 +1,16 @@
 package me.ppangya.wiki.framework.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
+@PropertySource(value = "classpath:properties/default.properties")
 public class RootApplicationConfig {
 
+	@Bean
+	public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 }
--- src/test/java/me/ppangya/wiki/framework/config/RootApplicationConfigTest.java
+++ src/test/java/me/ppangya/wiki/framework/config/RootApplicationConfigTest.java
@@ -6,11 +6,13 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Stream;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = RootApplicationConfig.class)
@@ -19,11 +21,21 @@ public class RootApplicationConfigTest {
 	private static final Logger logger = LoggerFactory.getLogger(RootApplicationConfigTest.class);
 
 	private @Autowired ApplicationContext applicationContext;
+	private @Value("${me.ppangya.wiki.backend}") String properties;
 
 	@Test
-	public void initRootApplicationConfig() {
+	public void initRootApplicationConfigTest() {
 		Assert.assertNotNull(applicationContext);
 		Date date = new Date(applicationContext.getStartupDate());
 		logger.info(String.valueOf(date));
+		String[] beans = applicationContext.getBeanDefinitionNames();
+		Optional<List<String>> beanListOptional = Optional.ofNullable(Arrays.asList(beans));
+		beanListOptional.map(Collection::stream).orElse(Stream.<String>empty()).forEach(logger::info);
+	}
+
+	@Test
+	public void propertiesSettingTest() {
+		Assert.assertNotNull(properties);
+		Assert.assertEquals("backend", properties);
 	}
 }
