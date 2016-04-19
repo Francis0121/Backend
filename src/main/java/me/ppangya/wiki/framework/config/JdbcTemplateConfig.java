abf96557c6ab13f939c2559f8d0beb8f27b11e76
Francis0121 <tjdrms0121@gmail.com>
2016-04-19 23:29:49 +0900

    Transcational AOP Type change Proxy-> AspectJ

--- src/main/java/me/ppangya/wiki/framework/config/JdbcTemplateConfig.java
+++ src/main/java/me/ppangya/wiki/framework/config/JdbcTemplateConfig.java
@@ -4,14 +4,16 @@ import org.apache.tomcat.jdbc.pool.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement()
+@EnableTransactionManagement(mode = AdviceMode.ASPECTJ)
 public class JdbcTemplateConfig {
 
 	private static final Logger logger = LoggerFactory.getLogger(JdbcTemplateConfig.class);
@@ -24,7 +26,7 @@ public class JdbcTemplateConfig {
 	}
 
 	@Bean
-	public DataSourceTransactionManager dataSourceTransactionManager() {
+	public PlatformTransactionManager dataSourceTransactionManager() {
 		return new DataSourceTransactionManager(dataSource);
 	}
 }
