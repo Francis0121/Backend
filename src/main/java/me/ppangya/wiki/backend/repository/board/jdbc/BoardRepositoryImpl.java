0466775af38788577c01dc97e3c658353f7b4c3e
Francis0121 <tjdrms0121@gmail.com>
2016-05-11 23:47:14 +0900

    Implement Jdbc Board

--- src/main/java/me/ppangya/wiki/backend/repository/board/jdbc/BoardRepositoryImpl.java
+++ src/main/java/me/ppangya/wiki/backend/repository/board/jdbc/BoardRepositoryImpl.java
@@ -6,6 +6,8 @@ import me.ppangya.wiki.backend.repository.entity.Board;
 import me.ppangya.wiki.framework.annotation.OrmConditional;
 import me.ppangya.wiki.framework.constant.SystemProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
@@ -24,27 +26,39 @@ public class BoardRepositoryImpl implements BoardRepository {
 	private static final String INSERT_SQL = "INSERT INTO BOARD (title) VALUES (?)";
 	private static final String UPDATE_SQL = "UPDATE BOARD SET title = ? WHERE board_id= ?";
 	private static final String DELETE_SQL = "DELETE FROM BOARD WHERE board_id = ?";
-	private static final String FIND_ONE_SQL = "SELECT board_id, title FROM BOARD WHERE board_id = ?";
+	private static final String FIND_ONE_SQL = "SELECT board_id AS boardId, title FROM BOARD WHERE board_id = ?";
 
 	@Override
 	public <S extends Board> Board save(S board) {
-		log.debug("Save Parameter : {}", board);
-		KeyHolder keyHolder = new GeneratedKeyHolder();
-		jdbcTemplate.update(connection -> {
-			PreparedStatement ps = connection.prepareStatement(INSERT_SQL, new String[]{"board_id"});
-			ps.setString(1, board.getTitle());
-			return ps;
-		}, keyHolder);
-		return new Board(keyHolder.getKey().longValue(), board.getTitle());
+		Optional<Board> boardOptional = findOne(board.getBoardId());
+		Board findBoard = boardOptional.orElseGet(() -> {
+			KeyHolder keyHolder = new GeneratedKeyHolder();
+			jdbcTemplate.update(connection -> {
+				PreparedStatement ps = connection.prepareStatement(INSERT_SQL, new String[]{"board_id"});
+				ps.setString(1, board.getTitle());
+				return ps;
+			}, keyHolder);
+			return new Board(keyHolder.getKey().longValue(), board.getTitle());
+		});
+		findBoard.setTitle(board.getTitle());
+		jdbcTemplate.update(UPDATE_SQL, findBoard.getTitle(), findBoard.getBoardId());
+		return findBoard;
 	}
 
 	@Override
 	public void delete(Board board) {
-
+		Optional<Board> boardOptional = findOne(board.getBoardId());
+		boardOptional.ifPresent(findBoard -> jdbcTemplate.update(DELETE_SQL, board.getBoardId()));
 	}
 
 	@Override
-	public Optional<Board> findOne(Long id) {
-		return null;
+	public Optional<Board> findOne(Long boardId) {
+		Board board = null;
+		try {
+			board = jdbcTemplate.queryForObject(FIND_ONE_SQL, new Object[]{boardId}, new BeanPropertyRowMapper<>(Board.class));
+		} catch (EmptyResultDataAccessException e) {
+			log.debug("board is not found");
+		}
+		return Optional.ofNullable(board);
 	}
 }
--- src/test/java/me/ppangya/wiki/backend/repository/board/jdbc/JdbcBoardRepositoryImplTest.java
+++ src/test/java/me/ppangya/wiki/backend/repository/board/jdbc/JdbcBoardRepositoryImplTest.java
@@ -1,31 +1,70 @@
 package me.ppangya.wiki.backend.repository.board.jdbc;
 
+import lombok.extern.slf4j.Slf4j;
 import me.ppangya.wiki.backend.repository.board.BoardRepository;
 import me.ppangya.wiki.backend.repository.entity.Board;
 import me.ppangya.wiki.test.annotation.JdbcTransactionalTest;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Optional;
+
+@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @JdbcTransactionalTest
 public class JdbcBoardRepositoryImplTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(JdbcBoardRepositoryImplTest.class);
-
 	private @Autowired BoardRepository boardRepository;
 
+	private Board initBoard;
+
+	@Before
+	public void before() {
+		initBoard = new Board(null, "Board Title");
+	}
+
 	@Test
-	public void saveTest() {
-		Board board = new Board(null, "Board Title");
-		board = boardRepository.save(board);
+	public void saveTest_insertEntity() {
+		Board board = boardRepository.save(initBoard);
 		Assert.assertNotNull(board);
 		Assert.assertNotNull(board.getBoardId());
 		Assert.assertNotNull(board.getTitle());
-		logger.debug("Insert : {}", board);
+	}
+
+	@Test
+	public void saveTest_updateEntity() {
+		Board board = boardRepository.save(initBoard);
+		board.setTitle("Modify Board Title");
+
+		Board modifyBoard = boardRepository.save(board);
+		Assert.assertNotNull(modifyBoard);
+		Assert.assertEquals(board.getBoardId(), modifyBoard.getBoardId());
+		Assert.assertEquals(board.getTitle(), modifyBoard.getTitle());
+	}
+
+	@Test
+	public void findOneTest() {
+		Long boardId = boardRepository.save(initBoard).getBoardId();
+
+		Optional<Board> boardOptional = boardRepository.findOne(boardId);
+		Board board = boardOptional.orElse(null);
+		Assert.assertNotNull(board);
+		Assert.assertEquals(boardId, board.getBoardId());
+		Assert.assertEquals(initBoard.getTitle(), board.getTitle());
+	}
+
+	@Test
+	public void deleteTest() {
+		Board board = boardRepository.save(initBoard);
+		Long boardId = board.getBoardId();
+		boardRepository.delete(board);
+
+		Optional<Board> boardOptional = boardRepository.findOne(boardId);
+		Board findBoard = boardOptional.orElse(null);
+		Assert.assertNull(findBoard);
 	}
 }
